b31e7fbe5e76eb11f8cb6b668e7892744fb89c85
Ivan Aguayo <leo.ivan.aguayo@gmail.com>
2021-03-01 23:08:39 -0800

    finished objective

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,19 +18,26 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Pets
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.imageLoader
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -38,90 +45,230 @@ import dev.chrisbanes.accompanist.coil.CoilImage
 import okio.buffer
 import okio.source
 
+object PuppyData {
+    var items = mutableListOf<PuppyEntity>()
+}
+
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    fun initPuppyData() {
         val listType = object : TypeToken<List<PuppyEntity>>() {}.type
         val puppyData: List<PuppyEntity> =
             assets.open("puppies.json").source().buffer().readUtf8()
                 .let { Gson().fromJson(it, listType) }
-        val imageLoader = this.imageLoader
+        PuppyData.items.clear()
+        PuppyData.items.addAll(puppyData)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initPuppyData()
         setContent {
             MyTheme {
-                PuppyList(puppyData)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {
+                            Text("Adopt a Pupper")
+                        })
+                    }
+                ) {
+                    navigationRoot()
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun navigationRoot() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "puppyList") {
+        composable("puppyList") { PuppyList(navController) }
+        composable(
+            "puppy/{puppyId}",
+            arguments = listOf(navArgument("puppyId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            PuppyDetail(
+                navController,
+                backStackEntry.arguments?.getInt("puppyId")
+            )
         }
     }
 }
 
 // Start building your app here!
 @Composable
-fun PuppyList(puppyData: List<PuppyEntity>) {
+fun PuppyList(navController: NavController) {
     Surface(color = MaterialTheme.colors.background) {
         val stateList = rememberLazyListState()
         LazyColumn(
             state = stateList,
-            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 0.dp)
+            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp)
         ) {
-            items(puppyData) { puppyEntity ->
-                PuppyCard(puppyEntity.name, puppyEntity.img, puppyEntity.id)
+            items(PuppyData.items) { puppyEntity ->
+                PuppyCard(puppyEntity.name, puppyEntity.gender, puppyEntity.img) {
+                    navController.navigate("puppy/${puppyEntity.id}")
+                }
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(8.dp)
+                )
             }
         }
 
     }
 }
 
+@Composable
+fun PuppyCard(name: String, gender: String, img: String, clickListener: () -> Unit) {
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        elevation = 4.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = clickListener)
+    ) {
+        Column {
+            CoilImage(
+                data = img, contentDescription = "Puppy Image", fadeIn = true,
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillWidth, alignment = Alignment.TopCenter
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(modifier = Modifier.weight(1F, true)) {
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.h5,
+                        modifier = Modifier.padding(16.dp, 8.dp, 16.dp, 0.dp)
+                    )
+                    Text(
+                        text = gender,
+                        style = MaterialTheme.typography.subtitle2,
+                        modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 8.dp)
+                    )
+                }
+                Icon(Icons.Outlined.Pets, "pets icon", modifier = Modifier.padding(end = 16.dp))
+            }
+        }
+    }
+}
 
 @Composable
-fun PuppyCard(name: String, img: String, id: Int) {
-    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+fun PuppyDetail(navController: NavController, puppyId: Int?) {
+    val puppyEntity = PuppyData.items.filter { entry -> entry.id == puppyId }.first()
+    Column() {
         CoilImage(
-            data = img, contentDescription = "Puppy Image", fadeIn = true,
-            modifier = Modifier.size(120.dp)
+            data = puppyEntity.img,
+            contentDescription = "Puppy Image",
+            fadeIn = true,
+            contentScale = ContentScale.Fit,
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
         )
-        Spacer(Modifier.width(10.dp))
-        Text(text = name, style = MaterialTheme.typography.subtitle1)
+        Spacer(Modifier.height(4.dp))
+        Column(
+            Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(8.dp)) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+            ) {
+                Text(
+                    puppyEntity.name,
+                    modifier = Modifier.weight(1F),
+                    style = MaterialTheme.typography.h5
+                )
+                Text(puppyEntity.age, style = MaterialTheme.typography.subtitle2)
+            }
+            Spacer(Modifier.height(4.dp))
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+            ) {
+                Text(
+                    puppyEntity.gender,
+                    modifier = Modifier.weight(1F),
+                    style = MaterialTheme.typography.subtitle2
+                )
+                Text(puppyEntity.breed)
+            }
+            Spacer(Modifier.height(4.dp))
+            Text(
+                puppyEntity.bio,
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+        }
     }
 }
 
 //@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        PuppyList(
-            arrayListOf(
-                PuppyEntity(
-                    id = 1,
-                    name = "Max",
-                    img = "",
-                    age = "2m",
-                    bio = "",
-                    breed = "",
-                    gender = "Male",
-                    weight = "1lb"
-                )
-            )
+    PuppyData.items.add(
+        PuppyEntity(
+            id = 1,
+            name = "Max",
+            img = "",
+            age = "2m",
+            bio = "",
+            breed = "",
+            gender = "Male",
+            weight = "1lb"
         )
+    )
+    MyTheme(darkTheme = true) {
+        navigationRoot()
     }
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
-    MyTheme {
-        PuppyList(
-            arrayListOf(
-                PuppyEntity(
-                    id = 1,
-                    name = "Max",
-                    img = "",
-                    age = "2m",
-                    bio = "",
-                    breed = "",
-                    gender = "Male",
-                    weight = "1lb"
-                )
-            )
+    PuppyData.items.add(
+        PuppyEntity(
+            id = 1,
+            name = "Max",
+            img = "",
+            age = "2m",
+            bio = "",
+            breed = "",
+            gender = "Male",
+            weight = "1lb"
         )
+    )
+    MyTheme {
+        navigationRoot()
+    }
+}
+
+@Preview("Detail", widthDp = 360, heightDp = 640)
+@Composable()
+fun DetailPreview() {
+    PuppyData.items.add(
+        PuppyEntity(
+            id = 1,
+            name = "Max",
+            img = "",
+            age = "2m",
+            bio = "",
+            breed = "",
+            gender = "Male",
+            weight = "1lb"
+        )
+    )
+    MyTheme {
+        val navController = rememberNavController()
+        PuppyDetail(navController = navController, puppyId = 1)
     }
 }
